a4d84ff048bc373f3446506ad74b20a5f28d5d42
jojje123213 <lizhuoyun06@gmail.com>
2025-05-17 14:28:05 +0800

    Update gameApi.py
    已除去print

--- gameApi.py
+++ gameApi.py
@@ -16,9 +16,9 @@ def play_turn(human_pos: Tuple[int, int]) -> Tuple[Tuple[int, int], List[List[in
     ai_pos = get_move_ai()
     move(const.AI, ai_pos)
 
-    # 控制台显示
-    print_board(gobang_board)
-
-    #
+    # # 控制台显示（已除去）
+    # print_board(gobang_board)
+    
+    #返回棋盘
     board_copy = [row[:] for row in gobang_board]
     return ai_pos, board_copy
